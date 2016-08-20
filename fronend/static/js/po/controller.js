8420bfab05fc54b6f7e43d4ed345d55ced79fa70
Shernaz S <shernaz.s@outlook.com>
2016-08-20 17:48:33 +0200

    added all-products page

--- fronend/static/js/po/controller.js
+++ fronend/static/js/po/controller.js
@@ -1,35 +1,27 @@
 angular.module('webshop.controllers', [])
 .controller("webshop-controller", function( $scope ) {
-
+	$scope.allProducts = [];
 	$scope.initFunction = function() {
 		console.log("controller init");
-		
 		$scope.getProducts();
 	}
-
 	$scope.getProducts = function() {
 		var config = {
       		type: "GET",
-      		url: "http://fairfrog-alok416.c9users.io/webshop/get_products",
+      		url: "http://localhost:8000/shop/get_products",
       		dataType: 'json',
       		data: {
-        		// 'longitude' : $scope.longitude,
-        		// 'latitude' : $scope.latitude,
       		},
       	success: function( response ) {
 		   	console.log( "success1");
 				if( response.status == 1) {
 					console.log( "success");
-					//$scope.address = response.address;
-					//$scope.$apply();
-					console.log( response.products_list );
-					//$scope.setMapMarker($scope.latitude, $scope.longitude, response.address );
+					$scope.allProducts = response.products_list;
+					$scope.$apply();
 				}
 		   }
-    };
+    	};
     $.ajax(config);
    }
-
- 
 })
 
