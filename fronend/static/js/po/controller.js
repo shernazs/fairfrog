b00f5f36d0d65ee42d8d50fbfbe454a7e51e4b80
Shernaz S <shernaz.s@outlook.com>
2016-08-31 23:49:30 +0200

    integration of homepage and product page

--- fronend/static/js/po/controller.js
+++ fronend/static/js/po/controller.js
@@ -5,46 +5,13 @@ angular.module('webshop.controllers', [])
   $scope.advertorialProducts =[];
   $scope.popularProducts = [];
 	$scope.loadedproducts = 0;
-
+  $scope.homepage = 1;
 
 	$scope.initFunction = function() {
 		console.log("controller init");
-		$scope.getProducts();
     $scope.getAdvertorialProducts();
     $scope.getPopularProducts();
 	}
-
-	// $scope.loadMore = function() {
- //        for (var i = 0; i < 15; i++) {
- //            $scope.items.push($scope.allProducts[$scope.loadedproducts]);
- //            $scope.loadedproducts++;
-            
-            
- //        }
- //        console.log("in loadmore")
- //        console.log($scope.items)
- //        $scope.$apply();
- //    };
-
-	$scope.getProducts = function() {
-		var config = {
-      		type: "GET",
-      		url: "http://localhost:8000/shop/get_products",
-      		dataType: 'json',
-      		data: {
-      		},
-      	success: function( response ) {
-		   	console.log( "success1");
-				if( response.status == 1) {
-					console.log( "success");
-					$scope.allProducts = response.products_list;
-					//$scope.loadMore();
-					$scope.$apply();
-				}
-		   }
-    	};
-    $.ajax(config);
-   }
 
    $scope.getAdvertorialProducts = function() {
     var config = {
@@ -87,7 +54,8 @@ angular.module('webshop.controllers', [])
     $.ajax(config);
    }
 
-  $scope.getCategories = function(categories) {
+  $scope.getProducts = function(categories) {
+
     console.log("in get categories");
 
     caturl = "http://127.0.0.1:8000/get_products?cat=" + categories
@@ -104,6 +72,7 @@ angular.module('webshop.controllers', [])
           console.log( "success");
           $scope.allProducts = response.products_list;
           console.log( $scope.allProducts);
+          $scope.homepage = null;
           $scope.$apply();
         }
        }
